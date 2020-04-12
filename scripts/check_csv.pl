b924e5da4ef31f057305084d88544dbce353eccd
Dan Zeman <zeman@ufal.mff.cuni.cz>
2020-04-12 19:43:21 +0200

    Reorganized code.

--- scripts/check_csv.pl
+++ scripts/check_csv.pl
@@ -20,7 +20,7 @@ GetOptions
 my @headers = ();
 my $nf;
 my $iline = 0;
-my %h;
+my @data; # the table, without the header line
 while(<>)
 {
     $iline++;
@@ -63,24 +63,56 @@ while(<>)
         {
             print STDERR ("Line $iline ($f[1]): Expected $nf fields, found $n.\n");
         }
-        # Remember observed features and values.
-        for(my $i = 3; $i <= $#headers; $i++)
-        {
-            $f[$i] = '_UNDEF_' if(!defined($f[$i]));
-            $h{$headers[$i]}{$f[$i]}++;
-        }
+        push(@data, \@f);
     }
 }
-# Print statistics of features and their values.
-for(my $i = 3; $i <= $#headers; $i++)
+# Hash the observed features and values.
+my %h;
+foreach my $language (@data)
 {
-    print("$headers[$i]\n\n");
-    my @values = sort {my $r = $h{$headers[$i]}{$b} <=> $h{$headers[$i]}{$a}; unless($r) {$r = $a cmp $b} $r} (keys(%{$h{$headers[$i]}}));
-    foreach my $value (@values)
+    # Remember observed features and values.
+    for(my $i = 3; $i <= $#headers; $i++)
     {
-        print("  $value\t$h{$headers[$i]}{$value}\n");
+        # There seem to be multiple ways of indicating an unknown value. Unify them.
+        $language->[$i] = '?' if(!defined($language->[$i]) || $language->[$i] eq 'nan' || $language->[$i] eq '?');
+        $h{$headers[$i]}{$language->[$i]}++;
     }
-    print("\n");
+}
+list_features_and_values(\@headers, \%h);
+
+
+
+#------------------------------------------------------------------------------
+# Prints statistics of features and their values.
+#------------------------------------------------------------------------------
+sub list_features_and_values
+{
+    my $headers = shift; # array ref
+    my $h = shift; # hash ref
+    for(my $i = 3; $i <= $#{$headers}; $i++)
+    {
+        print("$headers->[$i]\n\n");
+        my @values = sort {my $r = $h->{$headers->[$i]}{$b} <=> $h->{$headers->[$i]}{$a}; unless($r) {$r = $a cmp $b} $r} (keys(%{$h->{$headers->[$i]}}));
+        # Compute probabilities of known values.
+        my $sum = 0;
+        foreach my $value (@values)
+        {
+            unless($value eq '?')
+            {
+                $sum += $h->{$headers->[$i]}{$value}
+            }
+        }
+        foreach my $value (@values)
+        {
+            my $p = '';
+            unless($value eq '?')
+            {
+                $p = $h->{$headers->[$i]}{$value} / $sum;
+            }
+            print("  $value\t$h->{$headers->[$i]}{$value}\t$p\n");
+        }
+        print("\n");
+    }
 }
 
 
