addf0b2b7c9988e402b76d6d16c7989d3cf39844
Dan Zeman <zeman@ufal.mff.cuni.cz>
2020-06-22 11:39:10 +0200

    Vstupně-výstupní funkce přesunuty do samostatného modulu.

---
+++ scripts/Sigtypio.pm
@@ -0,0 +1,259 @@
+package Sigtypio;
+
+use utf8;
+use namespace::autoclean;
+
+use Carp;
+
+
+
+#------------------------------------------------------------------------------
+# Takes the column headers (needed because of their order) and the current
+# language-feature hash with the newly predicted values and prints them as
+# a CSV file to STDOUT.
+#------------------------------------------------------------------------------
+sub write_csv
+{
+    my $data = shift; # hash ref
+    my @headers = map {escape_commas($_)} (@{$data->{infeatures}});
+    print(join(',', @headers), "\n");
+    my @lcodes = sort {$data->{lh}{$a}{index} <=> $data->{lh}{$b}{index}} (@{$data->{lcodes}});
+    foreach my $l (@lcodes)
+    {
+        my @values = map
+        {
+            # If we modified some values of some input features, restore the
+            # original values now.
+            my $v = exists($data->{restore}{$l}{$_}) ? $data->{restore}{$l}{$_} : $data->{lh}{$l}{$_};
+            escape_commas($v);
+        }
+        (@{$data->{infeatures}});
+        print(join(',', @values), "\n");
+    }
+}
+
+
+
+#------------------------------------------------------------------------------
+# Takes a value of a CSV cell and makes sure that it is enclosed in quotation
+# marks if it contains a comma.
+#------------------------------------------------------------------------------
+sub escape_commas
+{
+    my $string = shift;
+    if($string =~ m/"/) # "
+    {
+        die("A CSV value must not contain a double quotation mark");
+    }
+    if($string =~ m/,/)
+    {
+        $string = '"'.$string.'"';
+    }
+    return $string;
+}
+
+
+
+#------------------------------------------------------------------------------
+# Reads the input CSV (comma-separated values) file: either from STDIN, or from
+# files supplied as arguments. Returns a list of column headers and a list of
+# table rows (both as array refs).
+#------------------------------------------------------------------------------
+sub read_csv
+{
+    # @_ can optionally contain the list of files to read from.
+    # If it does not exist, @ARGV will be tried. If it is empty, read from STDIN.
+    my $myfiles = 0;
+    my @oldargv;
+    if(scalar(@_) > 0)
+    {
+        $myfiles = 1;
+        @oldargv = @ARGV;
+        @ARGV = @_;
+    }
+    my @headers = ();
+    my $nf;
+    my $iline = 0;
+    my @data; # the table, without the header line
+    my $rcomma = chr(12289); # IDEOGRAPHIC COMMA
+    my $id = 0;
+    my %feature_names;
+    while(<>)
+    {
+        $iline++;
+        s/\r?\n$//;
+        # In the non-original (comma-separated) file, we must distinguish commas inside quotes from those outside.
+        unless($original)
+        {
+            $_ = process_quoted_commas($_, $rcomma);
+        }
+        my @f = ();
+        if(scalar(@headers)==0)
+        {
+            # In the original dev.csv, the headers are separated by one or more spaces, while the rest is separated by tabulators!
+            if($original)
+            {
+                @f = split(/\s+/, $_);
+                # Insert the column for the numeric id that we use in our format.
+                unshift(@f, '');
+            }
+            else
+            {
+                @f = split(/,/, $_);
+            }
+            @headers = map {restore_commas($_, $rcomma)} (@f);
+            $nf = scalar(@headers);
+        }
+        else
+        {
+            if($original)
+            {
+                @f = split(/\t/, $_);
+                # Insert the column for the numeric id that we use in our format.
+                unshift(@f, $id++);
+            }
+            else
+            {
+                @f = split(/,/, $_);
+            }
+            @f = map {restore_commas($_, $rcomma)} (@f);
+            my $n = scalar(@f);
+            # The number of values may be less than the number of columns if the trailing columns are empty.
+            # However, the number of values must not be greater than the number of columns (which would happen if a value contained the separator character).
+            if($n > $nf)
+            {
+                # Assume that it can be fixed by joining the last column with the extra columns.
+                my $ntojoin = $n-$nf+1;
+                print STDERR ("Line $iline ($f[1]): Expected $nf fields, found $n. Joining the last $ntojoin fields.\n");
+                $f[$nf-1] = join('', @f[($nf-1)..($n-1)]);
+                splice(@f, $nf);
+            }
+            # The original format has 8 columns, now 9 because we added the numeric index.
+            # The ninth column contains all the features. Remember their names.
+            if($original && scalar(@f)==9)
+            {
+                my @features = map {s/=.*//; $_} (split(/\|/, $f[8]));
+                foreach my $feature (@features)
+                {
+                    $feature_names{$feature}++;
+                }
+            }
+            push(@data, \@f);
+        }
+    }
+    # If we read the original format, split the features so that each has its own column.
+    if($original)
+    {
+        my @fnames = sort(keys(%feature_names));
+        splice(@headers, $#headers, 1, @fnames);
+        foreach my $row (@data)
+        {
+            my @features = split(/\|/, pop(@{$row}));
+            my %features;
+            foreach my $fv (@features)
+            {
+                if($fv =~ m/^(.+?)=(.+)$/)
+                {
+                    $features{$1} = $2;
+                }
+            }
+            foreach my $f (@fnames)
+            {
+                if(defined($features{$f}))
+                {
+                    push(@{$row}, $features{$f});
+                }
+                else
+                {
+                    push(@{$row}, 'nan');
+                }
+            }
+        }
+    }
+    if($myfiles)
+    {
+        @ARGV = @oldargv;
+    }
+    if(scalar(@headers) > 0 && $headers[0] eq '')
+    {
+        $headers[0] = 'index';
+    }
+    # We may want to add new combined features but we will not want to output them;
+    # therefore we must save the original list of features.
+    my @infeatures = @headers;
+    my %data =
+    (
+        'infeatures' => \@infeatures,
+        'features'   => \@headers,
+        'table'      => \@data,
+        'nf'         => scalar(@headers),
+        'nl'         => scalar(@data)
+    );
+    return %data;
+}
+
+
+
+#------------------------------------------------------------------------------
+# Substitutes commas surrounded by quotation marks. Removes quotation marks.
+#------------------------------------------------------------------------------
+sub process_quoted_commas
+{
+    my $string = shift;
+    my $rcomma = shift; # the replacement
+    # We will replace inside commas by another character.
+    # Perform a sanity check first: the replacement character must not appear in the input.
+    if($string =~ m/$rcomma/)
+    {
+        die("Want to use '$rcomma' as a replacement for comma but it occurs in the input.");
+    }
+    my @inchars = split(//, $string);
+    my @outchars = ();
+    my $inside = 0;
+    foreach my $char (@inchars)
+    {
+        # Assume that the quotation mark always has the special function and never occurs as a part of the value.
+        if($char eq '"')
+        {
+            if($inside)
+            {
+                $inside = 0;
+            }
+            else
+            {
+                $inside = 1;
+            }
+        }
+        else
+        {
+            if($inside && $char eq ',')
+            {
+                push(@outchars, $rcomma);
+            }
+            else
+            {
+                push(@outchars, $char);
+            }
+        }
+    }
+    $string = join('', @outchars);
+    return $string;
+}
+
+
+
+#------------------------------------------------------------------------------
+# Once the input line has been split to fields, the commas in the fields can be
+# restored.
+#------------------------------------------------------------------------------
+sub restore_commas
+{
+    my $string = shift;
+    my $rcomma = shift; # the replacement
+    $string =~ s/$rcomma/,/g;
+    return $string;
+}
+
+
+
+1;
--- scripts/train_and_predict_dz.pl
+++ scripts/train_and_predict_dz.pl
@@ -15,6 +15,32 @@ binmode(STDIN, ':utf8');
 binmode(STDOUT, ':utf8');
 binmode(STDERR, ':utf8');
 use Getopt::Long;
+# We need to tell Perl where to find my IO module.
+# If this does not work, you can put the script together with Sigtypio.pm
+# in a folder of you choice, say, /home/joe/scripts, and then
+# invoke Perl explicitly telling it where the modules are:
+# perl -I/home/joe/scripts /home/joe/scripts/train_and_predict_dz.pl ...
+BEGIN
+{
+    use Cwd;
+    my $path = $0;
+    $path =~ s/\\/\//g;
+    #print STDERR ("path=$path\n");
+    my $currentpath = getcwd();
+    $currentpath =~ s/\r?\n$//;
+    $libpath = $currentpath;
+    if($path =~ m:/:)
+    {
+        $path =~ s:/[^/]*$:/:;
+        chdir($path);
+        $libpath = getcwd();
+        chdir($currentpath);
+    }
+    $libpath =~ s/\r?\n$//;
+    #print STDERR ("libpath=$libpath\n");
+}
+use lib $libpath;
+use Sigtypio;
 
 my %config;
 # Possible debug levels:
@@ -96,7 +122,7 @@ GetOptions
 
 my $data_folder = 'data';
 print STDERR ("Reading the training data...\n");
-my %traindata = read_csv("$data_folder/train_y.csv");
+my %traindata = Sigtypio::read_csv("$data_folder/train_y.csv");
 print STDERR ("Found $traindata{nf} headers.\n");
 print STDERR ("Found $traindata{nl} language lines.\n");
 print STDERR ("Hashing the features and their cooccurrences...\n");
@@ -108,10 +134,10 @@ if($config{print_hi})
     print_hi(\%traindata);
 }
 print STDERR ("Reading the development data...\n");
-my %devdata = read_csv("$data_folder/dev_x.csv");
+my %devdata = Sigtypio::read_csv("$data_folder/dev_x.csv");
 # Read the gold standard development data. It will help us with debugging and error analysis.
 print STDERR ("Reading the development gold standard data...\n");
-my %devgdata = read_csv("$data_folder/dev_y.csv");
+my %devgdata = Sigtypio::read_csv("$data_folder/dev_y.csv");
 print STDERR ("Found $devdata{nf} headers.\n");
 print STDERR ("Found $devdata{nl} language lines.\n");
 my $ndevlangs = $devdata{nl};
@@ -125,9 +151,9 @@ print_qm_analysis(\%devdata);
 print STDERR ("Predicting the masked features...\n");
 predict_masked_features(\%traindata, \%devdata, \%devgdata);
 print STDERR ("Writing the completed file...\n");
-write_csv(\%devdata);
+Sigtypio::write_csv(\%devdata);
 print STDERR ("Reading the blind test data...\n");
-my %testdata = read_csv("$data_folder/test_x.csv");
+my %testdata = Sigtypio::read_csv("$data_folder/test_x.csv");
 print STDERR ("Found $testdata{nf} headers.\n");
 print STDERR ("Found $testdata{nl} language lines.\n");
 my $ntestlangs = $testdata{nl};
@@ -140,7 +166,7 @@ print_qm_analysis(\%testdata);
 print STDERR ("Predicting the masked features...\n");
 predict_masked_features(\%traindata, \%testdata);
 print STDERR ("Writing the completed file...\n");
-write_csv(\%testdata);
+Sigtypio::write_csv(\%testdata);
 
 
 
@@ -855,255 +881,6 @@ sub modify_features
 #==============================================================================
 # Input and output functions.
 #==============================================================================
-
-
-
-#------------------------------------------------------------------------------
-# Takes the column headers (needed because of their order) and the current
-# language-feature hash with the newly predicted values and prints them as
-# a CSV file to STDOUT.
-#------------------------------------------------------------------------------
-sub write_csv
-{
-    my $data = shift; # hash ref
-    my @headers = map {escape_commas($_)} (@{$data->{infeatures}});
-    print(join(',', @headers), "\n");
-    my @lcodes = sort {$data->{lh}{$a}{index} <=> $data->{lh}{$b}{index}} (@{$data->{lcodes}});
-    foreach my $l (@lcodes)
-    {
-        my @values = map
-        {
-            # If we modified some values of some input features, restore the
-            # original values now.
-            my $v = exists($data->{restore}{$l}{$_}) ? $data->{restore}{$l}{$_} : $data->{lh}{$l}{$_};
-            escape_commas($v);
-        }
-        (@{$data->{infeatures}});
-        print(join(',', @values), "\n");
-    }
-}
-
-
-
-#------------------------------------------------------------------------------
-# Takes a value of a CSV cell and makes sure that it is enclosed in quotation
-# marks if it contains a comma.
-#------------------------------------------------------------------------------
-sub escape_commas
-{
-    my $string = shift;
-    if($string =~ m/"/) # "
-    {
-        die("A CSV value must not contain a double quotation mark");
-    }
-    if($string =~ m/,/)
-    {
-        $string = '"'.$string.'"';
-    }
-    return $string;
-}
-
-
-
-#------------------------------------------------------------------------------
-# Reads the input CSV (comma-separated values) file: either from STDIN, or from
-# files supplied as arguments. Returns a list of column headers and a list of
-# table rows (both as array refs).
-#------------------------------------------------------------------------------
-sub read_csv
-{
-    # @_ can optionally contain the list of files to read from.
-    # If it does not exist, @ARGV will be tried. If it is empty, read from STDIN.
-    my $myfiles = 0;
-    my @oldargv;
-    if(scalar(@_) > 0)
-    {
-        $myfiles = 1;
-        @oldargv = @ARGV;
-        @ARGV = @_;
-    }
-    my @headers = ();
-    my $nf;
-    my $iline = 0;
-    my @data; # the table, without the header line
-    my $rcomma = chr(12289); # IDEOGRAPHIC COMMA
-    my $id = 0;
-    my %feature_names;
-    while(<>)
-    {
-        $iline++;
-        s/\r?\n$//;
-        # In the non-original (comma-separated) file, we must distinguish commas inside quotes from those outside.
-        unless($original)
-        {
-            $_ = process_quoted_commas($_, $rcomma);
-        }
-        my @f = ();
-        if(scalar(@headers)==0)
-        {
-            # In the original dev.csv, the headers are separated by one or more spaces, while the rest is separated by tabulators!
-            if($original)
-            {
-                @f = split(/\s+/, $_);
-                # Insert the column for the numeric id that we use in our format.
-                unshift(@f, '');
-            }
-            else
-            {
-                @f = split(/,/, $_);
-            }
-            @headers = map {restore_commas($_, $rcomma)} (@f);
-            $nf = scalar(@headers);
-        }
-        else
-        {
-            if($original)
-            {
-                @f = split(/\t/, $_);
-                # Insert the column for the numeric id that we use in our format.
-                unshift(@f, $id++);
-            }
-            else
-            {
-                @f = split(/,/, $_);
-            }
-            @f = map {restore_commas($_, $rcomma)} (@f);
-            my $n = scalar(@f);
-            # The number of values may be less than the number of columns if the trailing columns are empty.
-            # However, the number of values must not be greater than the number of columns (which would happen if a value contained the separator character).
-            if($n > $nf)
-            {
-                # Assume that it can be fixed by joining the last column with the extra columns.
-                my $ntojoin = $n-$nf+1;
-                print STDERR ("Line $iline ($f[1]): Expected $nf fields, found $n. Joining the last $ntojoin fields.\n");
-                $f[$nf-1] = join('', @f[($nf-1)..($n-1)]);
-                splice(@f, $nf);
-            }
-            # The original format has 8 columns, now 9 because we added the numeric index.
-            # The ninth column contains all the features. Remember their names.
-            if($original && scalar(@f)==9)
-            {
-                my @features = map {s/=.*//; $_} (split(/\|/, $f[8]));
-                foreach my $feature (@features)
-                {
-                    $feature_names{$feature}++;
-                }
-            }
-            push(@data, \@f);
-        }
-    }
-    # If we read the original format, split the features so that each has its own column.
-    if($original)
-    {
-        my @fnames = sort(keys(%feature_names));
-        splice(@headers, $#headers, 1, @fnames);
-        foreach my $row (@data)
-        {
-            my @features = split(/\|/, pop(@{$row}));
-            my %features;
-            foreach my $fv (@features)
-            {
-                if($fv =~ m/^(.+?)=(.+)$/)
-                {
-                    $features{$1} = $2;
-                }
-            }
-            foreach my $f (@fnames)
-            {
-                if(defined($features{$f}))
-                {
-                    push(@{$row}, $features{$f});
-                }
-                else
-                {
-                    push(@{$row}, 'nan');
-                }
-            }
-        }
-    }
-    if($myfiles)
-    {
-        @ARGV = @oldargv;
-    }
-    if(scalar(@headers) > 0 && $headers[0] eq '')
-    {
-        $headers[0] = 'index';
-    }
-    # We may want to add new combined features but we will not want to output them;
-    # therefore we must save the original list of features.
-    my @infeatures = @headers;
-    my %data =
-    (
-        'infeatures' => \@infeatures,
-        'features'   => \@headers,
-        'table'      => \@data,
-        'nf'         => scalar(@headers),
-        'nl'         => scalar(@data)
-    );
-    return %data;
-}
-
-
-
-#------------------------------------------------------------------------------
-# Substitutes commas surrounded by quotation marks. Removes quotation marks.
-#------------------------------------------------------------------------------
-sub process_quoted_commas
-{
-    my $string = shift;
-    my $rcomma = shift; # the replacement
-    # We will replace inside commas by another character.
-    # Perform a sanity check first: the replacement character must not appear in the input.
-    if($string =~ m/$rcomma/)
-    {
-        die("Want to use '$rcomma' as a replacement for comma but it occurs in the input.");
-    }
-    my @inchars = split(//, $string);
-    my @outchars = ();
-    my $inside = 0;
-    foreach my $char (@inchars)
-    {
-        # Assume that the quotation mark always has the special function and never occurs as a part of the value.
-        if($char eq '"')
-        {
-            if($inside)
-            {
-                $inside = 0;
-            }
-            else
-            {
-                $inside = 1;
-            }
-        }
-        else
-        {
-            if($inside && $char eq ',')
-            {
-                push(@outchars, $rcomma);
-            }
-            else
-            {
-                push(@outchars, $char);
-            }
-        }
-    }
-    $string = join('', @outchars);
-    return $string;
-}
-
-
-
-#------------------------------------------------------------------------------
-# Once the input line has been split to fields, the commas in the fields can be
-# restored.
-#------------------------------------------------------------------------------
-sub restore_commas
-{
-    my $string = shift;
-    my $rcomma = shift; # the replacement
-    $string =~ s/$rcomma/,/g;
-    return $string;
-}
 
 
 
